d23293ccedd62e8152f01b32fe8fb361841b47a0
JuanGraciano <juanpedro7juanpedro7@gmail.com>
2017-10-06 23:22:31 -0400

    init

---
+++ crop.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+
+def drawContour(frame):
+        # grey = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
+        value = (15, 15)
+        blurred = cv2.GaussianBlur(frame, value, 0)
+        _, thresh1 = cv2.threshold(blurred,50,255, cv2.THRESH_BINARY)
+        image, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        height, width, _ = frame.shape
+        min_x, min_y = width, height
+        max_x = max_y = 0
+
+        # computes the bounding box for the contour, and draws it on the frame,
+        hand
+        for contour in contours:
+            (x,y,w,h) = cv2.boundingRect(contour)
+            min_x, max_x = min(x, min_x), max(x+w, max_x)
+            min_y, max_y = min(y, min_y), max(y+h, max_y)
+            if w > 90 and h > 90:
+                bMax = bMin = 10
+                if y - bMin <= 0 and x - bMin <= 0:
+                    bMin = -1
+                if y+h + bMax >= frame.shape[0] and x+w + bMax >= frame.shape[1]:
+                    bMax = 0
+                hand = frame[int(y-bMin):int(y+h+bMax), int(x-bMin):int(x+w+bMax)]
+             	
+                # cv2.imshow("hand", hand)
+
+        return hand
+
+def mergeImage(frame, width, height):
+ 
+    # Adjusting size
+    if frame.shape[0] > height:
+        hy = height/frame.shape[0]
+        hx = frame.shape[1]*hy
+        frame = cv2.resize(frame, (int(hx), int(height)), interpolation = cv2.INTER_CUBIC)
+        print("Change y", frame.shape)
+    if frame.shape[1] > width:
+        hx = width/frame.shape[1]
+        hy = frame.shape[0]*hx
+        frame = cv2.resize(frame, (int(width), int(hy)), interpolation = cv2.INTER_CUBIC)
+        print("Change x", frame.shape)
+    
+    # Mask
+    merge = np.zeros((int(width), int(height), 3))
+    
+    # White mask
+    merge.fill(255)
+    
+    # Putting the image in the middle
+    centerMerge = width / 2
+    centerFrame = frame.shape[1] / 2
+    x_offset = centerMerge - centerFrame
+    y_offset = height - frame.shape[0]
+
+    # Merge
+    merge[int(y_offset):int(y_offset+frame.shape[0]), int(x_offset):int(x_offset+frame.shape[1])] = frame
+
+    return merge
+
+def makeCanny_WB(frame, imgCanny):
+    imgWB = cv2.threshold(frame,58,255,cv2.THRESH_BINARY)
+    out = imgWB[1] + imgCanny
+    return out
+
--- server.py
+++ server.py
@@ -1,35 +1,29 @@
-import restoreModel as model
-import socket
+import socket 
 import base64
-# from PIL import Image
-# import matplotlib.pyplot as plt
-# import matplotlib.image as mpimg
+import sys
+import time
+from datetime import timedelta
 
-ipServer = "10.0.0.10"
-portServer = 6789
+file = "A-test10.jpg"
 
+# with open(file, "rb") as imageFile:
+#   img = base64.b64encode(imageFile.read())
+#   img = "image-?" + img + "-?" + file
+#   print (img)
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-serverSocket.bind((ipServer, portServer))
+ip = "162.243.173.162"
+port = 5679
+message = "Hello, Server"
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# model.main()
-
-while True:
-    data, addr = serverSocket.recvfrom(58096)
-    # newdata = data[:4] + data[lenght(data):]
-    data1 = base64.b64decode(data)
-    fh = open("./try/try.png", "wb")
-    fh.write(data1)
-    fh.close()
-    # print(data)
-    if(data1):
-      # image = mpimg.imread('./try.png')
-      # plt.imshow(image)
-      # plt.show()
-      print("message", data1)
-      clientSocket.sendto(model.test().encode(), (addr))
-      print('okk')
-
+start_time = time.time()
+print("\nstart time " + str(start_time))
+droga = ('klk').encode()
+clientSocket.sendto(droga,(ip, port))
+data, addr = clientSocket.recvfrom(10000)
+end_time = time.time()
+print("\nend time " + str(end_time))
+diff = end_time - start_time
+print(data)
+print("Time elapsed: " + str(timedelta(seconds=int(round(diff)))))
